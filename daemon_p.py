aaa5b9c8ce1413626a59ff7a8fb7090670f3b291
Anton Navarka <anton_navarka@epam.com>
2021-07-29 20:36:50 +0300

    Add something

--- daemon_p.py
+++ daemon_p.py
@@ -1,39 +1,54 @@
 import os
+import subprocess
 import sys
+import time
 from time import sleep
+FIFO = "./namedpipe"
+STDOUT = "./logfile.log"
+STDERR = "./logfile.err"
+# TODO uncomment this
+# pid = os.fork()
+# if pid != 0:
+#     sys.exit(0)
 
-pid = os.fork()
-if pid != 0:
-    sys.exit(0)
+# os.setsid()
+# os.umask(0)
+# sys.stderr.flush()
+# sys.stdout.flush()
+try:
+    os.mkfifo(FIFO)
+except OSError:
+    pass
 
-os.chdir("~")
-os.setsid()
-os.umask(0)
-sys.stderr.flush()
-sys.stdout.flush()
-
-os.mkfifo("~/namedpipe")
-input = os.open("~/namedpipe", os.O_RDONLY)
-output = os.open("~/daemon_logfile.log", os.O_WRONLY)
-err = os.open("~/daemin_logfile.log", os.O_WRONLY)
-os.dup2(input, sys.stdin.fileno())
-os.dup2(output, sys.stdout.fileno())
-os.dup2(err, sys.stderr.fileno())
-
-while True:
-    sleep(2)
-    input_data = sys.stdin.read()
-    if input_data.isdigit():
-        # TODO Daemon shoud write to log the value of soft limit for "Max open files" of the PID
-        ...
-    elif input_data == "PACKAGES":
-        # TODO write top 5 packages installed in the system
-        ...
-    elif input_data.startswith("/dev/pts/"):
-        # TODO WRITE "Hello Sender" to the terminal /dev/pts/<NUM>. And write 'Sent message to user' to logfile
-        ...
-    elif input_data == "CLOSE":
-        # TODO Shut down daemon process with message "Shutting down" to log
-        ...
-
-os.unlink("/namedpipe")
+with open(FIFO) as fifo, open(STDOUT,"w+") as stdout, open(STDERR,"w+") as stderr:
+    while True:
+        input_data = fifo.read().strip("\n")
+        if not input_data:
+            sleep(1)
+        # TODO uncomment
+        # sys.stderr = stderr
+        # sys.stdout = stdout
+        if input_data.isdigit():
+            # TODO Daemon shoud write to log the value of soft limit for "Max open files" of the PID
+            command = f"cat /proc/{input_data}/limits | grep 'Max open files'"
+            output = str(subprocess.check_output(['/bin/bash', '-c', command]))
+            print(f"{time.time()} {output}")
+        elif input_data == "PACKAGES":
+            # TODO write top 5 packages installed in the system
+            print("packages")
+            output = str(subprocess.check_output(['/bin/bash','-c',"""apt list --installed | tail -n 5 | tr "\n" " " """]))
+            print(f"{time.time()} {output}")
+        elif input_data.startswith("/dev/pts/"):
+            # TODO WRITE "Hello Sender" to the terminal /dev/pts/<NUM>. And write 'Sent message to user' to logfile
+            print("dev/pts")
+            print(f"{time.time()} Message sent!")
+            subprocess.run(["/bin/bash", "-c", f"echo 'Hello Sender\n' > {input_data}"])
+        elif input_data == "CLOSE":
+            # TODO Shut down daemon process with message "Shutting down" to log
+            print("IM IN CLOSE")
+            print(f"{time.time()} Shutting down")
+            os.unlink("./namedpipe")
+            sys.exit(1)
+        print(f"NEW INPUT DATA:{input_data}")
+        print("NOWHERE")
+        sleep(5)
